17ce23bec5fdf97d9fa3e72a32780dab804a78f2
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-04-09 16:48:01 +0200

    addconsolelog

--- jv/script.js
+++ jv/script.js
@@ -6,13 +6,20 @@ const buttonEl = document.getElementById("button");
 
 buttonEl.addEventListener("click", () => {
   //   console.log("sono stato cliccato");
-  buttonEl.classList.replace("accendi", "spegni");
-
+  buttonEl.classList.toggle("bg-secondary");
+  buttonEl.classList.add("color-primary");
+  buttonEl.innerHTML = "spegni";
   if (lampEl.classList.toggle("d-none")) {
     lamponEl.classList.remove("d-none");
     lamponEl.classList.add("d-block");
   } else {
     lamponEl.classList.add("d-none");
   }
+  console.log(lampEl);
+  console.log(lamponEl);
+  console.log(buttonEl);
 });
 // la lampada si accende e spegne
+console.log(lampEl);
+console.log(lamponEl);
+console.log(buttonEl);
